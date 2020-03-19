b652437620e2177ba804dc04e889157d2b83950a
DongHuuk <49367132+DongHuuk@users.noreply.github.com>
2020-03-19 12:23:54 +0900

    entity ManyToOne
    @ManyToOne 설정 예제

---
+++ entityMtoO/Team.java
@@ -0,0 +1,42 @@
+package com.back.example.springboot.jpa.dongexample.entityMtoO;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+
+    private String name;
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
